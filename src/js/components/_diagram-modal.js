f3156ae9e994fa0998082692ea7f5dc8d094b1ec
whats0n <mrdvv@mail.ru>
2017-10-02 14:13:26 +0300

    update diagram modal position

--- src/js/components/_diagram-modal.js
+++ src/js/components/_diagram-modal.js
@@ -5,15 +5,52 @@ export default (function() {
   const $modalOpen = $('.js-diagram-modal-open');
   const $modalOverlay = $('.js-diagram-modal-overlay');
 
-  $modalOpen
-    .hover(function() { 
+
+
+  $modalOpen.each(function() {
+    const $this = $(this);
+    const $modalTarget = $this.find('.js-diagram-modal-target');
+    const $modalWindow = $this.find('.js-diagram-modal-window');
+
+    $this.hover(function() {
       if (window.matchMedia(`(max-width: ${1280}px)`).matches) return;
-      $(this).addClass(ACTIVE);
+      $this.addClass(ACTIVE);
       $modalOverlay.addClass(OPEN);
     }, function() {
       if (window.matchMedia(`(max-width: ${1280}px)`).matches) return;
-      $(this).removeClass(ACTIVE);
+      $this.removeClass(ACTIVE);
       $modalOverlay.removeClass(OPEN);
     });
+
+  	$modalTarget.on('mousemove', function(e) {
+
+  		const offset = $modalTarget.offset();
+  		const offsetTop = offset.top;
+  		const offsetLeft = offset.left;
+
+  		const scrollTop = $(window).scrollTop();
+  		const scrollLeft = $(window).scrollLeft();
+
+  		const mouseY = e.clientY;
+  		const mouseX = e.clientX;
+
+  		console.log(offsetTop, offsetLeft);
+  		console.log(mouseY, mouseX);
+
+  		const top = mouseY - (offsetTop - scrollTop);
+  		const left = mouseX - (offsetLeft - scrollLeft);
+
+  		console.log(scrollTop);
+  		console.log(scrollLeft);
+
+  		$modalWindow.css({
+  			top: `${top}px`,
+  			left: `${left}px`
+  		});
+
+  	});
+  
+  });
+
 
 })();
