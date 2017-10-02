38528a2c05afb0cac9cbe56159533765e3045a11
Валерий <kaktuz34@gmail.com>
2017-10-02 21:22:29 +0300

    added tooltip

--- src/js/components/_diagram-modal.js
+++ src/js/components/_diagram-modal.js
@@ -1,56 +1,35 @@
-import { OPEN, ACTIVE } from '../_constants';
+import { OPEN, ACTIVE, BODY } from '../_constants';
+import { TOUCH } from '../_utils';
+
+import tooltipster from 'tooltipster';
+require('tooltipster-follower');
 
 export default (function() {
 
-  const $modalOpen = $('.js-diagram-modal-open');
+  const $modalOpen = $('[data-diagram-modal="parent"]');
   const $modalOverlay = $('.js-diagram-modal-overlay');
-
-
 
   $modalOpen.each(function() {
     const $this = $(this);
-    const $modalTarget = $this.find('.js-diagram-modal-target');
-    const $modalWindow = $this.find('.js-diagram-modal-window');
 
     $this.hover(function() {
-      if (window.matchMedia(`(max-width: ${1280}px)`).matches) return;
+      if (TOUCH()) return;
       $this.addClass(ACTIVE);
       $modalOverlay.addClass(OPEN);
     }, function() {
-      if (window.matchMedia(`(max-width: ${1280}px)`).matches) return;
+      if (TOUCH()) return;
       $this.removeClass(ACTIVE);
       $modalOverlay.removeClass(OPEN);
     });
 
-  	// $modalTarget.on('mousemove', function(e) {
-      
-    //    	if (window.matchMedia(`(max-width: ${1280}px)`).matches) return;
-
-  	// 	const offset = $modalTarget.offset();
-  	// 	const offsetTop = offset.top;
-  	// 	const offsetLeft = offset.left;
-
-  	// 	const scrollTop = $(window).scrollTop();
-  	// 	const scrollLeft = $(window).scrollLeft();
-
-  	// 	const mouseY = e.clientY;
-  	// 	const mouseX = e.clientX;
-
-  	// 	console.log(offsetTop, offsetLeft);
-  	// 	console.log(mouseY, mouseX);
-
-  	// 	const top = mouseY - (offsetTop - scrollTop);
-  	// 	const left = mouseX - (offsetLeft - scrollLeft);
-
-  	// 	console.log(scrollTop);
-  	// 	console.log(scrollLeft);
-
-  	// 	$modalWindow.css({
-  	// 		top: `${top}px`,
-  	// 		left: `${left}px`
-  	// 	});
-
-  	// });
+    if (TOUCH()) return;
+    $modalOpen.tooltipster({
+      anchor: 'top-left',
+      minWidth: 400,
+      maxWidth: 400,
+      offset: [0, -25],
+      plugins: ['follower']
+    });
   
   });
 
