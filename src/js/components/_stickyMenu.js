630068bb9ff36c699bf1b8b31867490f603dc6ae
Валерий <kaktuz34@gmail.com>
2017-10-07 16:22:19 +0300

    upd

--- src/js/components/_stickyMenu.js
+++ src/js/components/_stickyMenu.js
@@ -1,11 +1,10 @@
 import { WIN, SHOW, FIXED } from '../_constants';
 
-;(function() {
-  let header = $('.js-header');
+;(function() { let header = $('.js-header');
   let lastScrollTop = 0;
-  let menuFixed = (position) => { (position > 0) ? header.addClass(FIXED) : header.removeClass(FIXED); };
   WIN.scroll( function(event) {
     let scrollPosition = $(this).scrollTop();
+    if (WIN.width() > 768) return;
     (scrollPosition > lastScrollTop) 
       ? header.removeClass(SHOW)
       : header.addClass(SHOW);
@@ -13,5 +12,6 @@ import { WIN, SHOW, FIXED } from '../_constants';
     menuFixed(scrollPosition);
   });
 
+  let menuFixed = (position) => { (position > 0) ? header.addClass(FIXED) : header.removeClass(FIXED); };
 
 })();
