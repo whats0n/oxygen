ac4d1bf58894f380118e33b9b935aa7034132cc6
Валерий <kaktuz34@gmail.com>
2017-10-10 20:01:38 +0300

    update fps time

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,34 @@
+var fps = { 
+  startTime : 0, 
+  frameNumber : 0, 
+  getFPS : function() { 
+    this.frameNumber++; 
+    var d = new Date().getTime(), 
+      currentTime = ( d - this.startTime ) / 1000, 
+      result = Math.floor( ( this.frameNumber / currentTime ) ); 
+    if( currentTime > 1 ) { 
+      this.startTime = new Date().getTime(); this.frameNumber = 0; 
+    } return result; 
+  } 
+};
+
+let bbb = 0;
+let timeOut;
+function gameLoop() { 
+  timeOut = setTimeout( gameLoop,1000 / 60 ); 
+  bbb = fps.getFPS();
+}
+window.onload = gameLoop;
+$(document).ready(function() {
+
+  setTimeout(function() {
+    alert(bbb);
+    if (bbb<60) {
+      clearTimeout(timeOut);
+    }
+  }, 3000);
+});
+
 import common from './_common';
 import { DOC } from './_constants';
 
--- src/js/components/_clouds.js
+++ src/js/components/_clouds.js
@@ -137,36 +137,6 @@ import Rellax from 'rellax';
     window.addEventListener( 'mousemove', (e) => onMouseMove(e, world) );
   });
 
-  var fps = { 
-    startTime : 0, 
-    frameNumber : 0, 
-    getFPS : function() { 
-      this.frameNumber++; 
-      var d = new Date().getTime(), 
-        currentTime = ( d - this.startTime ) / 1000, 
-        result = Math.floor( ( this.frameNumber / currentTime ) ); 
-      if( currentTime > 1 ) { 
-        this.startTime = new Date().getTime(); this.frameNumber = 0; 
-      } return result; 
-    } 
-  };
-
-  let bbb = 0;
-  let timeOut;
-  function gameLoop() { 
-    timeOut = setTimeout( gameLoop,1000 / 60 ); 
-    bbb = fps.getFPS();
-  }
-  window.onload = gameLoop;
-  $(document).ready(function() {
-    
-    setTimeout(function() {
-      alert(bbb);
-      if (bbb<60) {
-        clearTimeout(timeOut);
-      }
-    }, 2000);
-  });
   let frame;
   const update = () => {
 
