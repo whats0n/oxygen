bf30bb4e9613a739d1a18004989b120468c7dbbd
Валерий <kaktuz34@gmail.com>
2017-10-17 21:09:36 +0300

    upd

--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-import common from './_common';
-import { DOC } from './_constants';
-
-DOC.ready(common);
-
-
--- src/js/components/_scrollWidth.js
+++
@@ -1,6 +0,0 @@
-export const SCROLL_WIDTH = () => {
-  let documentWidth = parseInt(document.documentElement.clientWidth),
-    windowsWidth = parseInt(window.innerWidth),
-    scrollbarWidth = windowsWidth - documentWidth;
-  return scrollbarWidth;
-};
