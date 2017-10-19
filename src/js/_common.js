a7516390a02c4f22005bd1f69ae68d635d8d3c38
Валерий <kaktuz34@gmail.com>
2017-10-19 17:14:22 +0300

    update th popup and faq

--- src/js/_common.js
+++ src/js/_common.js
@@ -1,7 +1,16 @@
-import { TOUCH } from './_utils';
+import { TOUCH, SCROLL_TO } from './_utils';
 import { BODY } from './_constants';
 import './components';
 import svg4everybody from 'svg4everybody';
 
 svg4everybody();
 if (!TOUCH()) BODY.addClass('no-touch');
+
+// SCROLL_TO
+const scrollTo = $('.js-scroll-to');
+scrollTo.click(function() {
+  var id = $(this).attr('href'),
+    posTop = $(id).offset().top;
+  SCROLL_TO(posTop);
+  return false;
+});
